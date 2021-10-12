e3e7bc94991da9b153cc761ef268052384f73ece
santushtisharma10 <sharmasantushti1012@gmail.com>
2021-10-12 23:45:05 +0530

    bootstrap added

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,23 +1,28 @@
 import React, { useState } from "react";
 
+// styling and more info needs to be added
+
 export default function Display({ vis, info, repo }) {
 
     const conSrc = "http://ghchart.rshah.org/" + info.login
     const gitSrc = "https://github-readme-stats.vercel.app/api?username=" + info.login + "&show_icons=true&theme=chartreuse-dark"
-    console.log("here in display")
+    
     return vis === 1 ?  (
         <div>
 
             <div className="row">
-                <div className="col-3-md">
-                    <img src={info.avatar_url} />
+                <div className="col-md-6" style={{backgroundColor: "red"}}>
+                    
                     <h1> {info.name} </h1>
                     <h3> {info.login}</h3>
                     <p>{info.bio}</p>
                 </div>
-                <div className="col-3-md">
+                <div className="col-md-6">
+                <h1> {info.name} </h1>
+                    <h3> {info.login}</h3>
+                    <p>{info.bio}</p>{/*
                     <img src={gitSrc} />
-                    <img src={conSrc} alt="2016rshah's Github chart" />
+                    <img src={conSrc} alt="2016rshah's Github chart" />*/}
                 </div>
 
             </div>
